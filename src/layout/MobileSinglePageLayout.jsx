e9480a6e159da8d887e8884b9df1d433cf3afb48
Adewale Mudasiru <mudasiruluqmaan+coding@gmail.com>
2026-03-05 08:23:11 +0100

    removed unsued variable

--- src/layout/MobileSinglePageLayout.jsx
+++ src/layout/MobileSinglePageLayout.jsx
@@ -2,7 +2,6 @@ import Home from "../pages/home/Home";
 import About from "../pages/about/About";
 import Projects from "../pages/projects/Projects";
 import Contact from "../pages/contact/Contact";
-import { twMerge } from "tailwind-merge";
 
 /**
  * Mobile single-page layout that displays all sections on one scrollable page
